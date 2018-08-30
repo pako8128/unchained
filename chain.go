7b7108cddd3815832c16dc8ed4b62a3f2df169a1
PaKo <pako8128@protonmail.com>
2018-08-30 02:05:40 +0200

    added the Blockchain definition

---
+++ chain.go
@@ -0,0 +1,11 @@
+package unchained
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prev_block := bc.blocks[len(bc.blocks)-1]
+	new_block := NewBlock(data, prev_block.Hash)
+	bc.blocks = append(bc.blocks, new_block)
+}
