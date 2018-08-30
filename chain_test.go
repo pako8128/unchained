3ca653c93dbc12f9b4be2747af8ced8532a83ce6
PaKo <pako8128@protonmail.com>
2018-08-30 02:54:32 +0200

    there you have a proof of work (like hashcash)

---
+++ chain_test.go
@@ -0,0 +1,20 @@
+package unchained
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestChain(t *testing.T) {
+	bc := NewBlockchain()
+
+	bc.AddBlock("Send 1 BTC to Ivan")
+	bc.AddBlock("Send 2 more BTC to Ivan")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+}
