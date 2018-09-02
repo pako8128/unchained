d5b9eb9d303ce4e766e8eaafca0513bb39847d0d
PaKo <pako8128@protonmail.com>
2018-09-02 06:03:17 +0200

    rewrote the test to fit the new API

--- chain.go
+++ chain.go
@@ -13,7 +13,7 @@ type Blockchain struct {
 func (bc *Blockchain) AddBlock(data string) {
 	var lastHash []byte
 
-	err := bc.db.View(func(tx *bolt.Tx) error {
+	bc.db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(blocksBucket))
 		lastHash = b.Get([]byte("l"))
 
@@ -22,10 +22,10 @@ func (bc *Blockchain) AddBlock(data string) {
 
 	block := NewBlock(data, lastHash)
 
-	err = bc.db.Update(func(tx *bolt.Tx) error {
+	bc.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(blocksBucket))
-		err = b.Put(block.Hash, block.Serialize())
-		err = b.Put([]byte("l"), block.Hash)
+		b.Put(block.Hash, block.Serialize())
+		b.Put([]byte("l"), block.Hash)
 		bc.tip = block.Hash
 
 		return nil
@@ -34,16 +34,16 @@ func (bc *Blockchain) AddBlock(data string) {
 
 func NewBlockchain() *Blockchain {
 	var tip []byte
-	db, err := bolt.Open(dbFile, 0600, nil)
+	db, _ := bolt.Open(dbFile, 0600, nil)
 
-	err = db.Update(func(tx *bolt.Tx) error {
+	db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(blocksBucket))
 
 		if b == nil {
 			genesis := NewGenesisBlock()
-			b, err = tx.CreateBucket([]byte(blocksBucket))
-			err = b.Put(genesis.Hash, genesis.Serialize())
-			err = b.Put([]byte("l"), genesis.Hash)
+			b, _ = tx.CreateBucket([]byte(blocksBucket))
+			b.Put(genesis.Hash, genesis.Serialize())
+			b.Put([]byte("l"), genesis.Hash)
 			tip = genesis.Hash
 		} else {
 			tip = b.Get([]byte("l"))
--- chain_test.go
+++ chain_test.go
@@ -1,8 +1,8 @@
 package unchained
 
 import (
-	// "fmt"
-	// "strconv"
+	"fmt"
+	"strconv"
 	"testing"
 )
 
@@ -11,13 +11,16 @@ func TestChain(t *testing.T) {
 
 	bc.AddBlock("Send 1 BTC to Ivan")
 	bc.AddBlock("Send 2 more BTC to Ivan")
-	//
-	// for _, block := range bc.blocks {
-	// fmt.Printf("Prev. hash: %x\n", block.PrevHash)
-	// fmt.Printf("Data: %s\n", block.Data)
-	// fmt.Printf("Hash: %x\n", block.Hash)
-	// pow := NewProofOfWork(block)
-	// fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-	// fmt.Println()
-	// }
+
+	iter := bc.Iterator()
+	block := iter.Next()
+	for string(block.Data) != "Genesis Block" {
+		fmt.Printf("Prev. hash: %x\n", block.PrevHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+		block = iter.Next()
+	}
 }
