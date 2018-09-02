99a159b40467d5a3de839b7e36c3dc6b52b81877
PaKo <pako8128@protonmail.com>
2018-09-02 19:42:41 +0200

    removed ugly Transaction representation from 'mining block' line

--- proof_of_work.go
+++ proof_of_work.go
@@ -42,7 +42,7 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 	var hash [32]byte
 	nonce := 0
 
-	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Transactions)
+	fmt.Println("Mining Block")
 	for nonce < maxNonce {
 		data := pow.prepareData(nonce)
 		hash = sha256.Sum256(data)
